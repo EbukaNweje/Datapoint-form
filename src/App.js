a2d6be82c41a369333432114958c3205f14c91e7
Nwej Ebuka <52475096+EbukaNweje@users.noreply.github.com>
2021-12-01 08:06:47 +0100

    My site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import DpForm from "./components/DpForm";
+import Header from "./component/Header"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
+import AllComponent from './component/AllComponent'
+import Course from "./component/Course/Course";
+import Footer from "./Footer/Footer"
+import HomePage from "./component/HomePage"
+import About from "./About/About"
+
 
 const App = () => {
   return (
-    <>
-      <DpForm/>
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="About" element={<About />} />
+         <Route path="/course" element={<Course />} />
+         <Route path="/course" element={<Course />} />
+      </Routes>
+      <AllComponent/>
+       <Footer/>
+      </Router>
   )
 }
 
